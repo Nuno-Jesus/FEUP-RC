36cf7ed486ca69d21db12a2e95dce88128636731
Nuno Jesus <up201905477@edu.fe.up.pt>
2022-12-21 18:23:26 +0000

    Documentation: documented entity.h

--- TP2/entity.h
+++ TP2/entity.h
@@ -5,18 +5,34 @@
 
 typedef struct entity_t
 {
-	char *user;
-	char *password;
+	char *user;		//Username
+	char *password;	//User's password
 	char *hostname;
 	char *path;
-	char *ip;
-	size_t port;
+	char *ip;		//Converted IP address of the host
+	size_t port;	//Port to be calculated after requesting passive mode
 }Entity;
 
+/**
+ * @brief Creates a new entity instance based on the parameters received
+ * @param user The username
+ * @param password The password of the user
+ * @param hostname The hostname
+ * @param path The path of the file to transfer
+ * @return An entity pointer on success, NULL otherwise
+ */
 Entity *entity_new (char *user, char *password, char *hostname, char *path);
 
+/**
+ * @brief Deletes the memory of Entity structure
+ * @param args The Entity structure to delete
+ */
 void entity_delete(Entity *args);
 
+/**
+ * @brief Prints the Entity strucures in a friendly way
+ * @param args The Entity structure to print
+ */
 void entity_print(Entity *args);
 
 #endif
