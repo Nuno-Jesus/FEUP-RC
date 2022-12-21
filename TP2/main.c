05d67aff22c5635a542cca74e07648231190c152
Nuno Jesus <up201905477@edu.fe.up.pt>
2022-12-21 22:27:23 +0000

    Code: applying regular expression to URL.

--- TP2/main.c
+++ TP2/main.c
@@ -19,23 +19,29 @@ char *getip(char *hostname)
 
 Entity *parse_url(char *link)
 {
-	if (strstr(link, "ftp://") != link)
-		print_error("parse_url", "missing \"ftp://\" at the beggining of the url");
+	regex_t regex;
+	regcomp(&regex, "ftp://.+:.+@.+/.+", REG_EXTENDED);
 
+	int status = regexec(&regex, link, 0, NULL, 0);
+	if (status == REG_NOMATCH)
+	{
+		printf("\n\t%sValid URL format: "
+			"ftp://<user>:<password>@<host>/<url-path>%s\n\n", BRED, RESET);
+		regfree(&regex);
+		return (NULL);
+	}
+	
 	char *dup = strmap(link + 6, &to_bar);
 	char *temp = strchr(dup , '/');
 	temp[0] = DELIMITER;
 	char **tokens = split(dup, DELIMITER);
 	Entity *args;
 
-	if (strchr(link + 6, '@') && strchr(link + 6, ':'))
-		args = entity_new(strdup(tokens[0]), strdup(tokens[1]), strdup(tokens[2]), strdup(tokens[3]));
-	else
-		args = entity_new(strdup("anonymous"), strdup("dummy"), strdup(tokens[0]), strdup(tokens[1]));
-
+	args = entity_new(strdup(tokens[0]), strdup(tokens[1]), strdup(tokens[2]), strdup(tokens[3]));
 	if (args)
 		args->ip = getip(args->hostname);
 	
+	regfree(&regex);
 	delete_matrix(tokens);
 	free(dup);
 
@@ -52,17 +58,14 @@ int main(int argc, char **argv)
 	char *response;
 
 	if (!(link = parse_url(argv[1])))
-	{
-		perror("malloc()");
 		exit(EXIT_FAILURE);
-	}
 	
 	entity_print(link); 
 
 	if ((fd = socket_open(link->ip, FTP_PORT)) < 0)
 	{
 		entity_delete(link);
-		print_error("socket_open", "Couldn't establish connection");
+		exit(EXIT_FAILURE);
 	}
 	
 	printf("\n\t%s_/=\\_/=\\_/=\\_ Connection established. _/=\\_/=\\_/=\\_%s\n\n", BYELLOW, RESET);
@@ -74,19 +77,19 @@ int main(int argc, char **argv)
 	if (code != CODE_SERVICE_READY)
 	{
 		entity_delete(link);
-		print_error("read_response", "Response code was not 220 (success code).");
+		exit(EXIT_FAILURE);
 	}
 
 	if (!login(fd, link))
 	{
 		entity_delete(link);
-		print_error("login", "Login error");
+		exit(EXIT_FAILURE);
 	}
 
 	if (!passive_mode(fd, link))
 	{
 		entity_delete(link);
-		print_error("passive_mode", "Whats");
+		exit(EXIT_FAILURE);
 	}
 
 	download(link, get_filename(link->path), fd);
@@ -94,7 +97,7 @@ int main(int argc, char **argv)
 	entity_delete(link);
 
 	if ((fd = socket_close(fd)) < 0)
-		print_error("socket_close", "Couldn't close connection");
+		exit(EXIT_FAILURE);
 
 	printf("\n\t%s_/=\\_/=\\_/=\\_ Connection closed. _/=\\_/=\\_/=\\_%s\n\n", BYELLOW, RESET);
 }
